c06d5a8c0faf10fac9132f700fd8ca08caedeb33
Victor Tang <victortang@lulutrip.com>
2016-09-28 12:10:35 +0800

    simplehtmldom_1_5

---
+++ src/SimpleHtmlDom.php
@@ -0,0 +1,20 @@
+<?php namespace Victor;
+
+require 'simplehtmldom_1_5'.DIRECTORY_SEPARATOR.'simple_html_dom.php';
+class SimpleHtmlDom {
+	
+	/**
+     * get html dom from file
+	 * @return \simplehtmldom_1_5\simple_html_dom
+	 */
+	static public function parseFile() {
+		return call_user_func_array ( '\simplehtmldom_1_5\file_get_html' , func_get_args() );
+	}
+	/**
+	 * get html dom from string
+	 * @return \simplehtmldom_1_5\simple_html_dom
+	 */
+	static public function parseStr() {
+		return call_user_func_array ( '\simplehtmldom_1_5\str_get_html' , func_get_args() );
+	}
+}
